81716940a3fb4a47de978a4d2dada64a3a85ae2f
Andrés Cuervo <andrescuervor@gmail.com>
2024-10-07 19:24:32 -0400

    Add slides claim changing slide data

--- what-is-folk/causal-islands-2024/bind.js
+++ what-is-folk/causal-islands-2024/bind.js
@@ -1,28 +1,31 @@
 let ws = 0;
 let folkname = "unset";
-const folknameEl = document.getElementById('folkname');
-const folknameDisplayEl = document.getElementById('folknameDisplay');
+const folknameEl = document.getElementById("folkname");
+const folknameDisplayEl = document.getElementById("folknameDisplay");
 
-const programListEl = spawnAndAttach('div');
+const programListEl = spawnAndAttach("div");
 programListEl.style = "font-family: monospace; white-space: pre-wrap;";
 
 function spawnAndAttach(tag) {
-    const el = document.createElement(tag);
-    document.body.appendChild(el);
-    return el;
+  const el = document.createElement(tag);
+  document.body.appendChild(el);
+  return el;
 }
 
 function connectToFolkName(_folkname) {
-    folkname = _folkname;
-    folknameDisplayEl.innerText = folkname;
-    ws = new FolkWS(document.getElementById('status'),`http://${folkname}.local:4273/ws`);
+  folkname = _folkname;
+  folknameDisplayEl.innerText = folkname;
+  ws = new FolkWS(
+    document.getElementById("status"),
+    `http://${folkname}.local:4273/ws`
+  );
 }
 
 connectToFolkName(folknameEl.value);
 
 folknameEl.onchange = (changeEvent) => {
-    connectToFolkName(changeEvent.target.value);
-}
+  connectToFolkName(changeEvent.target.value);
+};
 
 // TODO: Need to get this working in order to find out the name of e.g. all programs
 // demoCode = tcl`/someone/ claims /page/ has program code /c/`;
@@ -30,27 +33,30 @@ folknameEl.onchange = (changeEvent) => {
 //   - Try sharing program position, angle, etc. via Folk code.
 //   - .e.g `Claim [info hostname] has html "<h2>Blah</h2>\n<pre>some example code</pre>"`
 function watchAndReport(statement, reporterElement) {
-    // Parse statement for strings in foward slashes and pull out JSON keys in matches, accumalate and display that into the reporterElement
-    let keysInStatement = statement.match(/\/([^\/]+)\//g).map(e => e.slice(1, -1))
-    // console.log(keysInStatement)
+  // Parse statement for strings in foward slashes and pull out JSON keys in matches, accumalate and display that into the reporterElement
+  let keysInStatement = statement
+    .match(/\/([^\/]+)\//g)
+    .map((e) => e.slice(1, -1));
+  // console.log(keysInStatement)
 
-    ws.watchCollected(statement, matches => {
-        let reportString = `${folkname} has ${matches.length} match${matches.length === 1 ? '' : 'es'} for [${statement}]:`
+  ws.watchCollected(statement, (matches) => {
+    let reportString = `${folkname} has ${matches.length} match${
+      matches.length === 1 ? "" : "es"
+    } for [${statement}]:`;
 
-        if (matches.length === 0) {
-            globalMatches = []
-            // reporterElement.innerText = reportString
-            return
-        }
+    if (matches.length === 0) {
+      globalMatches = [];
+      //   reporterElement.innerText = reportString;
+      return;
+    }
 
-        for (let i = 0; i < matches.length; i++) {
-            let m = matches[i]
-            // console.table(m)
-            for (const key of keysInStatement) {
-                reportString += `   ${m[key]}`
-            }
-        }
-        // reporterElement.innerText = reportString
-        globalMatches = matches
-    });
+    for (let i = 0; i < matches.length; i++) {
+      let m = matches[i];
+      for (const key of keysInStatement) {
+        reportString += `   ${m[key]}`;
+      }
+    }
+    // reporterElement.innerText = reportString;
+    globalMatches = matches;
+  });
 }
--- what-is-folk/causal-islands-2024/makeSlides.js
+++ what-is-folk/causal-islands-2024/makeSlides.js
@@ -102,6 +102,7 @@ function parseGlobalMatches(globalMatches) {
   return result;
 }
 let displayedSlide = 0;
+
 function loop() {
   if (displayedSlide !== slideNumber) {
     console.log("displaying slide", slideNumber);
@@ -122,7 +123,12 @@ function loop() {
       newData.height = newData.height / HEIGHT;
       newData.n = globalMatches[0]["n"];
 
-      createFolkCard(newData);
+      slideNumber = newData.n;
+
+      if (newData.n === 60) {
+        console.log("making a new folk card");
+        createFolkCard(newData);
+      }
       globalMatches = undefined;
     }
   }
@@ -184,300 +190,6 @@ function createVideo(path, rotation = 0, mute = true) {
   }
 }
 
-function displaySlide_old(n = 0) {
-  // let container = document.getElementById("folk-container");
-  let slide = document.createElement("div");
-  containerEl.innerHTML = "";
-  switch (n) {
-    case 1:
-      // slide.innerHTML = '<h1 class="big"></h1>'
-      let title = createElementWithText(
-        "h1",
-        "Folk Computer</br>A Computer For The Real World"
-      );
-      title.classList.add("big");
-      slide.appendChild(title);
-
-      slide.appendChild(createElementWithText("h2", "Andr&eacute;s Cuervo"));
-
-      slide.appendChild(
-        createElementWithText(
-          "h2",
-          "<s>October 4, 2024</s> Septemeber 27, 2024"
-        )
-      );
-      break;
-    case 2:
-      slide.appendChild(createImage("./assets/laptop_on_desk.png"));
-      break;
-    case 3:
-      slide.appendChild(createImage("./assets/lenovo_thinkpad_clean.png", 3));
-      break;
-    case 4:
-      slide.appendChild(
-        createImage("./assets/lenovo_thinkpad_clean.png", 3.56)
-      );
-      slide.appendChild(createImage("./assets/apple.jpg", -4));
-      break;
-    case 5:
-      slide.appendChild(
-        createElementWithText(
-          "span",
-          "<a href='https://x.com/michael_nielsen/status/1787392849866362938/photo/1'>link to tweet</a>"
-        )
-      );
-      slide.appendChild(createImage("./assets/nielsen.png", -1));
-      break;
-    case 6:
-      slide.appendChild(
-        createElementWithText(
-          "span",
-          "<a href='https://x.com/andy_matuschak/status/1780030794779775146'>link to tweet</a>"
-        )
-      );
-      let andyDiv = document.createElement("div");
-      andyDiv.style.paddingLeft = "20%";
-      andyDiv.appendChild(createImage("./assets/matuschak.png"));
-      slide.appendChild(andyDiv);
-      break;
-    case 7:
-      slide.appendChild(
-        createElementWithText(
-          "span",
-          "<span class=big>Research</span> (<a href='https://en.wikipedia.org/wiki/Research#/media/File:Humanit%C3%A9s_Num%C3%A9riques.JPG'>link</a>)"
-        )
-      );
-      slide.appendChild(createImage("./assets/research.jpg"));
-      break;
-    case 8:
-      slide.appendChild(createImage("./assets/quest.jpg", -2));
-      break;
-    case 9:
-      slide.appendChild(createImage("./assets/quest.jpg", -2));
-      slide.appendChild(createImage("./assets/avp.jpg", 2));
-      break;
-    case 10:
-      slide.appendChild(createImage("./assets/mythic.png", -1));
-      break;
-    case 11:
-      slide.appendChild(createImage("./assets/mythic.png", -1));
-      slide.appendChild(createImage("./assets/daylight.png", 1));
-      break;
-    case 12:
-      slide.appendChild(createImage("./assets/devices.png"));
-      break;
-    case 13:
-      let bigText = document.createElement("div");
-      bigText.style.textAlign = "center";
-      bigText.style.paddingTop = "20%";
-
-      bigText.appendChild(
-        createElementWithText(
-          "h1",
-          "<span class=big>1. Spatial computing: useful but intangible</span>"
-        )
-      );
-      slide.appendChild(bigText);
-      break;
-    case 14:
-      let bigText2 = document.createElement("div");
-      bigText2.style.textAlign = "center";
-      bigText2.style.paddingTop = "20%";
-
-      bigText2.appendChild(
-        createElementWithText(
-          "h1",
-          "<span class=big>1. Spatial computing: useful but intangible</span>"
-        )
-      );
-      bigText2.appendChild(
-        createElementWithText(
-          "h1",
-          "<span class=big>2. Custom computers: tangible but isolated</span>"
-        )
-      );
-      slide.appendChild(bigText2);
-      break;
-    case 15:
-      // slide.appendChild(createImage("./assets/quest.jpg"))
-      slide.appendChild(displayDrawnSlides(1));
-      break;
-    case 16:
-      slide.appendChild(displayDrawnSlides(2));
-      break;
-    case 17:
-      slide.appendChild(displayDrawnSlides(3));
-      break;
-    case 18:
-      slide.appendChild(displayDrawnSlides(4));
-      break;
-    case 19:
-      slide.appendChild(displayDrawnSlides(5));
-      break;
-    case 20:
-      slide.appendChild(displayDrawnSlides(6));
-      break;
-    case 21:
-      slide.appendChild(displayDrawnSlides(7));
-      break;
-    case 22:
-      slide.appendChild(displayDrawnSlides(8));
-      break;
-    case 23:
-      slide.appendChild(displayDrawnSlides(9));
-      break;
-    case 24:
-      slide.appendChild(displayDrawnSlides(10));
-      break;
-    case 25:
-      slide.appendChild(displayDrawnSlides(11));
-      break;
-    case 26:
-      slide.appendChild(displayDrawnSlides(12));
-      break;
-    case 27:
-      slide.appendChild(displayDrawnSlides(13));
-      break;
-    case 28:
-      slide.appendChild(displayDrawnSlides(14));
-      break;
-    case 29:
-      slide.appendChild(displayDrawnSlides(15));
-      break;
-    case 30:
-      let padVideo = document.createElement("div");
-      padVideo.style.paddingLeft = "32%";
-      padVideo.style.marginTop = "-2%";
-      let video = createVideo("./assets/printing_a_program_2x.mp4");
-      video.playbackRate = 2.2;
-      padVideo.appendChild(video);
-      slide.appendChild(padVideo);
-      break;
-    case 31:
-      slide.appendChild(createImage("./assets/faries.gif"));
-      break;
-    case 32:
-      let padVideo_ = document.createElement("div");
-      padVideo_.style.paddingLeft = "32%";
-      padVideo_.style.marginTop = "-2%";
-      let video_ = slide.appendChild(createVideo("./assets/editor.mp4"));
-      video_.playbackRate = 3;
-      padVideo_.appendChild(video_);
-      slide.appendChild(padVideo_);
-      break;
-    case 33:
-      slide.appendChild(createImage("./assets/connecting_lines.jpg"));
-      break;
-    case 34:
-      let padVideo2 = document.createElement("div");
-      padVideo2.style.paddingLeft = "32%";
-      padVideo2.style.marginTop = "-2%";
-      let video2 = slide.appendChild(createVideo("./assets/animation.mp4"));
-      video2.playbackRate = 3;
-      padVideo2.appendChild(video2);
-      slide.appendChild(padVideo2);
-      break;
-    case 35:
-      slide.appendChild(createImage("./assets/jacob_text_association.gif"));
-      break;
-    case 36:
-      slide.appendChild(createImage("./assets/capture_deskshot.gif"));
-      break;
-    case 37:
-      slide.appendChild(createVideo("./assets/folk_naveen_opencv_sliders.mp4"));
-      break;
-    case 38:
-      slide.appendChild(createImage("./assets/blob.gif"));
-      break;
-    case 39:
-      slide.appendChild(createImage("./assets/jacob_pointer.gif"));
-      break;
-    case 40:
-      slide.appendChild(
-        createVideo("./assets/jessie_recurse_piano.mp4", 0, false)
-      );
-      break;
-    case 41:
-      slide.appendChild(createImage("./assets/pilot.png"));
-      break;
-    case 42:
-      slide.appendChild(
-        createElementWithText("h3", "folk-arc, Atlanta, Georgia, United States")
-      );
-      slide.appendChild(
-        createElementWithText(
-          "h3",
-          "folk-enjalot, Atlanta, Georgia, United States"
-        )
-      );
-      slide.appendChild(
-        createElementWithText("h3", "folk-opendot, Milan, Lombardy, Italy")
-      );
-      slide.appendChild(
-        createElementWithText("h3", "folk-dpip, Midvale, Utah, United States")
-      );
-      slide.appendChild(
-        createElementWithText(
-          "h3",
-          "folk-haip, Boston, Massachusetts, United States"
-        )
-      );
-      slide.appendChild(
-        createElementWithText("h3", "folk-zeca, São Paulo, Brazil")
-      );
-      slide.appendChild(
-        createElementWithText("h3", "folk-michaud, Toronto, Canada")
-      );
-      slide.appendChild(createElementWithText("h3", "folk-wwj, Sanya, China"));
-      break;
-    case 43:
-      slide.appendChild(createImage("./assets/future_1.png"));
-      break;
-    case 44:
-      slide.appendChild(createImage("./assets/future_2.png"));
-      break;
-    case 45:
-      slide.appendChild(createImage("./assets/future_3.png"));
-      break;
-    case 46:
-      slide.appendChild(createImage("./assets/future_4.png"));
-      break;
-    case 47:
-      slide.appendChild(createImage("./assets/future_5.png"));
-      break;
-    case 48:
-      slide.appendChild(createImage("./assets/future_6.png"));
-      break;
-    case 49:
-      slide.appendChild(createImage("./assets/future_7.png"));
-      break;
-    case 50:
-      slide.appendChild(createElementWithText("h2", "These Slides"));
-      slide.appendChild(createImage("./assets/gadget.jpg"));
-      break;
-    case 51:
-      slide.appendChild(createElementWithText("h1", "Andr&eacute;s Cuervo"));
-      slide.appendChild(createElementWithText("h1", "folk.computer/pilot"));
-      slide.appendChild(createElementWithText("h1", "Thank you"));
-      break;
-    case 99:
-      // let padVideo2 = document.createElement("div");
-      // padVideo2.style.paddingLeft = "32%";
-      // padVideo2.style.marginTop = "-2%";
-      // let video2 = slide.appendChild(createVideo("./assets/sound_cards.mp4"))
-      // video2.playbackRate = 2.2;
-      // padVideo2.appendChild(vide2)
-      // slide.appendChild(padVideo2)
-      break;
-    case 100:
-      slide.appendChild(displayDrawnSlides(2));
-      break;
-    default:
-      break;
-  }
-  containerEl.appendChild(slide);
-}
-
 function displayDrawnSlides(n = 1) {
   let imageContainer = document.createElement("div");
   imageContainer.style.position = "relative";
@@ -518,9 +230,7 @@ const slides = [
       title.classList.add("big");
       slide.appendChild(title);
       slide.appendChild(createElementWithText("h2", "Andr&eacute;s Cuervo"));
-      slide.appendChild(
-        createElementWithText("h2", "<s>October 4, 2024</s> September 27, 2024")
-      );
+      slide.appendChild(createElementWithText("h2", "October 8, 2024"));
       return slide;
     },
   },
@@ -636,7 +346,7 @@ const slides = [
       bigText.appendChild(
         createElementWithText(
           "h1",
-          "<span class=big>1. Spatial computing: useful but intangible</span>"
+          "<span class=big>1. Physical computing: useful but intangible</span>"
         )
       );
       return bigText;
@@ -650,7 +360,7 @@ const slides = [
       bigText2.appendChild(
         createElementWithText(
           "h1",
-          "<span class=big>1. Spatial computing: useful but intangible</span>"
+          "<span class=big>1. Physical computing: useful but intangible</span>"
         )
       );
       bigText2.appendChild(
@@ -799,4 +509,4 @@ document.body.addEventListener("keydown", (event) => {
   } else if (event.key === "f") {
     toggleFullscreen();
   }
-})
+});
